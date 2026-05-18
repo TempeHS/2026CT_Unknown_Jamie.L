3776b617a39d80273f4c2e9c9277b9ffd7092b6b
Jamie L <jamie.le7@education.nsw.gov.au>
2026-05-18 11:55:11 +1000

    WIP: inventory

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocity = moveInput * moveSpeed;
+
+    }
+
+    public void move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
